0191b4e663012e77825a6ea6502f9f4b479fc684
jared Higgins <hjared10@yahoo.com>
2020-04-11 14:43:52 -0600

    correctly added mlab

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_94kj8xbs:r0qt08ir6sq7vikk20vo6a52ik@ds141704.mlab.com:41704/heroku_94kj8xbs", {
-  useNewUrlParser: true,
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_94kj8xbs:r0qt08ir6sq7vikk20vo6a52ik@ds141704.mlab.com:41704/heroku_94kj8xbs", {
+  mongoose.connect("mongodb://heroku_94kj8xbs:r0qt08ir6sq7vikk20vo6a52ik@ds141704.mlab.com:41704/heroku_94kj8xbs", {
+   
+useNewUrlParser: true,
   useFindAndModify: false
 });
 
